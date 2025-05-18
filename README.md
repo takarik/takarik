cc7a500b1ab3eb28dd6cf7aa0d98ca3216e5ba6c
Sinan Keskin <sinan@kesk.in>
2025-05-19 02:46:40 +0300

    Update router DSL

--- README.md
+++ README.md
@@ -31,12 +31,96 @@ class HomeController < Takarik::BaseController
   end
 end
 
+class UsersController < Takarik::BaseController
+  actions :index, :show, :new, :create, :edit, :update, :destroy, :refresh
+
+  def index
+    render plain: "List of users"
+  end
+
+  def show
+    render plain: "User #{params["id"]}"
+  end
+
+  def new
+    render plain: "New user form"
+  end
+
+  def create
+    # Process form submission
+    render plain: "User created"
+  end
+
+  def edit
+    # Implementation needed
+  end
+
+  def update
+    # Implementation needed
+  end
+
+  def destroy
+    # Implementation needed
+  end
+
+  def refresh
+    # Custom member action
+    render plain: "Refreshing data for user #{params["id"]}"
+  end
+end
+
 app = Takarik::Application.new
-app.router.define do |map|
-  map.get "/", controller: HomeController, action: :index
+
+# Define routes
+app.router.define do
+  # Simple routes
+  get "/", controller: HomeController, action: :index
+
+  # Controller-scoped routes
+  map UsersController do
+    # RESTful resource routes (creates 7 standard routes)
+    resources :users do
+      # Custom member routes (operate on a specific resource)
+      member do
+        get "/refresh", action: :refresh
+      end
+    end
+
+    # With filtering
+    resources :posts, only: [:index, :show]
+    resources :comments, except: [:destroy]
+  end
 end
 
 app.run
+```
+
+### Resource Routing
+
+The `resources` method creates RESTful routes following Rails conventions:
+
+| HTTP Verb | Path | Controller#Action | Purpose |
+|-----------|------|-------------------|---------|
+| GET | /resources | #index | List all resources |
+| GET | /resources/new | #new | Form to create a new resource |
+| POST | /resources | #create | Create a resource |
+| GET | /resources/:id | #show | Show a specific resource |
+| GET | /resources/:id/edit | #edit | Form to edit a resource |
+| PATCH/PUT | /resources/:id | #update | Update a resource |
+| DELETE | /resources/:id | #destroy | Delete a resource |
+
+#### Custom Resource Routes
+
+You can add custom routes to resources:
+
+```crystal
+resources :users do
+  # Routes that operate on a specific user
+  member do
+    get "/refresh", action: :refresh        # Creates GET /users/:id/refresh
+    post "/send_email", action: :send_email # Creates POST /users/:id/send_email
+  end
+end
 ```
 
 ## Development
--- src/takarik/router.cr
+++ src/takarik/router.cr
@@ -10,31 +10,143 @@ module Takarik
     getter radix_tree : Radix::Tree(Hash(String, RouteInfo))
 
     @@instance : self = self.new
+    @current_controller : Takarik::BaseController.class | Nil = nil
+    @current_resource : String | Nil = nil
+    @current_scope : Symbol | Nil = nil
 
     def self.instance
       @@instance
     end
 
-    def self.define(&block : Router -> Nil)
-      yield self.instance
+    def self.define(&block)
+      with instance yield
     end
 
-    macro define_standard_http_routes
-      {% for verb in ["get", "post", "put", "patch", "delete"] %}
-        def {{verb.id}}(path_pattern : String, controller : Takarik::BaseController.class, action : Symbol)
-          add_route({{verb.upcase}}, path_pattern, controller, action)
+    {% for verb in ["get", "post", "put", "patch", "delete"] %}
+      def {{verb.id}}(path_pattern : String, controller : Takarik::BaseController.class, action : Symbol)
+        add_route({{verb.upcase}}, path_pattern, controller, action)
+      end
+    {% end %}
+
+    {% for verb in ["get", "post", "put", "patch", "delete"] %}
+      # Support for path strings
+      def {{verb.id}}(path_pattern : String, action : Symbol)
+        raise "Controller scope required. Use within a controller block." unless @current_controller
+
+        # Check if we're in a collection or member scope
+        path_prefix = case @current_scope
+        when :collection
+          "/#{@current_resource}"
+        when :member
+          "/#{@current_resource}/:id"
+        else
+          ""
         end
-      {% end %}
-    end
 
-    define_standard_http_routes
+        # Apply the prefix if needed
+        full_path = path_prefix.empty? ? path_pattern : "#{path_prefix}#{path_pattern}"
+
+        add_route({{verb.upcase}}, full_path, @current_controller.not_nil!, action)
+      end
+
+      # Rails-style symbol shortcuts
+      def {{verb.id}}(action : Symbol)
+        raise "Controller scope required. Use within a controller block." unless @current_controller
+        raise "Symbol shortcuts can only be used within collection or member blocks" unless @current_scope
+
+        path_prefix = case @current_scope
+        when :collection
+          "/#{@current_resource}/#{action}"
+        when :member
+          "/#{@current_resource}/:id/#{action}"
+        else
+          "/#{action}" # Should never happen due to the check above
+        end
+
+        add_route({{verb.upcase}}, path_prefix, @current_controller.not_nil!, action)
+      end
+    {% end %}
 
     def initialize
       @radix_tree = Radix::Tree(Hash(String, RouteInfo)).new
     end
 
-    def define(&block : Router -> Nil)
-      yield self
+    def define(&block)
+      with self yield
+    end
+
+    def map(controller : Takarik::BaseController.class, &block)
+      @current_controller = controller
+      with self yield
+      @current_controller = nil
+    end
+
+    def resources(resource_name : Symbol | String, **options)
+      raise "Controller scope required. Use within a controller block." unless @current_controller
+      setup_resource_routes(resource_name, @current_controller.not_nil!, options)
+    end
+
+    def resources(resource_name : Symbol | String, controller : Takarik::BaseController.class, **options)
+      setup_resource_routes(resource_name, controller, options)
+    end
+
+    # Version with block
+    def resources(resource_name : Symbol | String, **options, &block)
+      raise "Controller scope required. Use within a controller block." unless @current_controller
+      setup_resource_routes(resource_name, @current_controller.not_nil!, options)
+
+      # Create a resource scope for the block
+      resource = resource_name.to_s
+      prev_resource = @current_resource
+      @current_resource = resource
+
+      # Execute the block in this object's context
+      with self yield
+
+      # Restore the previous resource
+      @current_resource = prev_resource
+    end
+
+    # Version with controller and block
+    def resources(resource_name : Symbol | String, controller : Takarik::BaseController.class, **options, &block)
+      setup_resource_routes(resource_name, controller, options)
+
+      # Create a resource scope for the block
+      resource = resource_name.to_s
+      prev_controller = @current_controller
+      prev_resource = @current_resource
+
+      @current_controller = controller
+      @current_resource = resource
+
+      # Execute the block in this object's context
+      with self yield
+
+      # Restore the previous resource
+      @current_controller = prev_controller
+      @current_resource = prev_resource
+    end
+
+    def collection(&block)
+      raise "Collection can only be called within a resources block" unless @current_resource
+      resource = @current_resource.not_nil!
+      prev_scope = @current_scope
+      @current_scope = :collection
+
+      with self yield
+
+      @current_scope = prev_scope
+    end
+
+    def member(&block)
+      raise "Member can only be called within a resources block" unless @current_resource
+      resource = @current_resource.not_nil!
+      prev_scope = @current_scope
+      @current_scope = :member
+
+      with self yield
+
+      @current_scope = prev_scope
     end
 
     def match(request_method : String, request_path : String) : {RouteInfo, Hash(String, String)}?
@@ -59,7 +171,7 @@ module Takarik
       nil # No match found
     end
 
-    private def add_route(method : String | Symbol, path_pattern : String, controller : Takarik::BaseController.class, action : Symbol)
+    def add_route(method : String | Symbol, path_pattern : String, controller : Takarik::BaseController.class, action : Symbol)
       http_method = method.to_s.upcase
 
       route_info = RouteInfo.new(
@@ -84,6 +196,46 @@ module Takarik
       Log.debug { "Added route: #{http_method} #{path_pattern} -> #{controller.name}##{action}" }
 
       route_info
+    end
+
+    private def setup_resource_routes(resource_name : Symbol | String, controller : Takarik::BaseController.class, options)
+      raise "Controller parameter is required." unless controller
+
+      resource = resource_name.to_s
+      path_prefix = "/#{resource}"
+
+      # Set default actions
+      actions = %i(index new create show edit update destroy)
+
+      # Filter actions based on options
+      if only = options[:only]?
+        only_actions = only.is_a?(Array) ? only : [only.as(Symbol)]
+        actions = actions.select { |a| only_actions.includes?(a) }
+      elsif except = options[:except]?
+        except_actions = except.is_a?(Array) ? except : [except.as(Symbol)]
+        actions = actions.reject { |a| except_actions.includes?(a) }
+      end
+
+      # Generate routes for each action
+      actions.each do |action|
+        case action
+        when :index
+          add_route("GET", path_prefix, controller, :index)
+        when :new
+          add_route("GET", "#{path_prefix}/new", controller, :new)
+        when :create
+          add_route("POST", path_prefix, controller, :create)
+        when :show
+          add_route("GET", "#{path_prefix}/:id", controller, :show)
+        when :edit
+          add_route("GET", "#{path_prefix}/:id/edit", controller, :edit)
+        when :update
+          add_route("PUT", "#{path_prefix}/:id", controller, :update)
+          add_route("PATCH", "#{path_prefix}/:id", controller, :update)
+        when :destroy
+          add_route("DELETE", "#{path_prefix}/:id", controller, :destroy)
+        end
+      end
     end
   end
 end
